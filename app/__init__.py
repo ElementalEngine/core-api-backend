aaf599d4914e0f8ddd2eab18fdbd3e29cb520c9c
ProjectCisco <sga.accounts@proton.me>
2026-02-03 22:43:10 +0000

    refactor: restructure __init__.py to improve lazy loading of application

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,10 @@
-from app.main import app as application
+from __future__ import annotations
 
 __all__ = ["application"]
+
+def __getattr__(name: str):
+    if name == "application":
+        from app.main import app as application
+        
+        return application
+    raise AttributeError(name)
