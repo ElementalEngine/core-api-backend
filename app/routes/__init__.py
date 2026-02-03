9cd5039fc0cc940d7a9d25b6602ab402878209cd
ProjectCisco <sga.accounts@proton.me>
2026-02-03 22:58:28 +0000

    api: register stats router

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from app.routes.upload import router as upload_router
+
 from app.routes.matches import router as matches_router
+from app.routes.stats import router as stats_router
+from app.routes.upload import router as upload_router
 
 router = APIRouter()
 router.include_router(upload_router)
 router.include_router(matches_router)
+router.include_router(stats_router)
